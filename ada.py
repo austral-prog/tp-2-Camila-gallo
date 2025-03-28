4609cc4df46d3a4db9b818646b78974c70eed6a5
Camila-gallo <cgallopiva1@mail.austral.edu.ar>
2025-03-28 12:32:42 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name_lower= first_name.lower()+ " "+last_name.lower()
+    full_name= first_name.title() + " " + last_name.title()
+    full_name_upper= first_name.upper()+ " " + last_name.upper()
+    full_name_right= "        "+ full_name_lower
+    print(full_name_lower)
+    print(full_name)
+    print(full_name_upper)
+    print(full_name_right)
